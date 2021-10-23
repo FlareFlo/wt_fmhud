f84a77bd7d9353e325eca17096267eb5f6443278
FlareFlo <florianlipp17@gmail.com>
2021-10-23 18:45:33 +0200

    init commit

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,15 @@ use crate::response::Indicator;
 
 mod response;
 
+/*
+Routes:
+/mission.json
+/map_obj.json
+/map_info.json
+/indicators
+/state
+ */
+
 #[tokio::main]
 async fn main() {
     let reply = reqwest::get("http://localhost:8111/indicators").await.unwrap();
